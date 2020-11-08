391aec2994a1ccc94db12d17c50497b19df81750
Hafiz Muhammad Umar Hayat <52619520+MuhammadUmarHayat@users.noreply.github.com>
2020-11-08 15:40:52 +0500

    Add files via upload

---
+++ Code/AddCategory.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace OnlineEyeStore
+{
+    public partial class AddCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {//save
+            SqlConnection conn = new SqlConnection(DBClass.connectionString);
+
+            string query = "insert into Category(Title,Discription,Date) values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + DateTime.Now + "')";
+
+
+            SqlCommand sqlCmd = new SqlCommand(query, conn);
+            conn.Open();
+            sqlCmd.ExecuteNonQuery();// insertion into db
+            conn.Close();
+            Label1.Text = "Record is saved seccessfully";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //cancel
+
+            Response.Redirect("AddCategory.aspx");
+        }
+    }
+}
---
+++ Code/DBClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineEyeStore
+{
+    public class DBClass
+    {
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EyStoreFinalDB;Integrated Security=True;Pooling=False";
+    }
+}
